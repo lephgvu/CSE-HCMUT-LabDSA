e130664472372c416228f83fa758cb9261a55435
lephgvu <vuleephuong123@gmail.com>
2025-02-20 12:27:00 +0700

    edit file

--- Lab1/Recursion/printPattern.cpp
+++ Lab1/Recursion/printPattern.cpp
@@ -4,11 +4,16 @@ using namespace std;
 
 void printPattern(int n) 
 { 
-    /*  
-     * STUDENT ANSWER
-     */
+    if(n <= 0){
+        cout<<n;
+        return;
+    }
+    cout<<n<<" ";
+    printPattern(n-5);
+    cout<<" "<<n;
 }
+
 int main(){
-    printPattern(14);
+    printPattern(14); //14 9 4 -1 4 9 14
     return 0;
 }
