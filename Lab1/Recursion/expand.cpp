090fad679eee6e6d3f5d4fbcd6f16088bd0ac48a
lephgvu <vuleephuong123@gmail.com>
2025-03-03 16:58:28 +0700

    edit expand.cpp

--- Lab1/Recursion/expand.cpp
+++ Lab1/Recursion/expand.cpp
@@ -2,8 +2,28 @@
 #include <string>
 using namespace std;
 
+string multiply(string part, int count) {
+    return (count == 0) ? "" : part + multiply(part, count - 1);
+}
+
+int findClosingBracket(string s, int index, int count = 1) {
+    if (count == 0) return index - 1;
+    return findClosingBracket(s, index + 1, count + (s[index] == '(') - (s[index] == ')'));
+}
+
 string expand(string s) {
-    
+    if (s.empty()) return "";
+
+    if (islower(s[0])) return s[0] + expand(s.substr(1));
+
+    if (isdigit(s[0]) && s[1] == '(') {
+        int repeat = s[0] - '0';
+        int closePos = findClosingBracket(s, 2);
+        string expandedPart = expand(s.substr(2, closePos - 2));
+        return multiply(expandedPart, repeat) + expand(s.substr(closePos + 1));
+    }
+
+    return expand(s.substr(1));  // Bỏ qua ký tự không hợp lệ
 }
 
 int main(){
